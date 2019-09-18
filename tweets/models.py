c392dbbf36d1172beb421eec4c44a181ab6fb3c7
Rivikantha Herath <rivikantha@gmail.com>
2019-09-19 00:18:14 +0530

    make pyc files untracked created gitignore

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from user_profile.models import User
 
-# Create your models here.
+class Tweet(models.Model):
+
+	"""
+	Tweet Model
+	"""
+
+	user = models.ForeginKey(User)
+	
+
+	text = models.CharField(max_length=160)
+	created_date = models.DateTimeField(auto_now_add=True)
+	country = models.CharField(max_length=30)
+	is_active = models.BooleanField(default=True)
+
+
+
